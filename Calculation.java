829f3f6d7d2e52b840ade7dba9a960527235ba68
BhavsarPoonam1409 <bhavsarpoonam46@gmail.com>
2026-08-20 21:34:18 +0530

    Basic Calculatons program

---
+++ Calculation.java
@@ -0,0 +1,18 @@
+public class Calculation {
+    public static void main(String[]args){
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int substraction = a - b;
+        int multiply = a * b;
+        int divide = a / b;
+        int modulo = a % b;
+
+        System.out.println("sum is: "+sum);
+        System.out.println("substraction is: "+substraction);
+        System.out.println("multiply is: "+multiply);
+        System.out.println("divide is: "+divide);
+        System.out.println("modulo is: "+modulo);
+    }
+}
