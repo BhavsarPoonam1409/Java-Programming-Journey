e1b95007d96511216e9a76cf4e797a30e707799b
BhavsarPoonam1409 <bhavsarpoonam46@gmail.com>
2026-08-20 21:18:09 +0530

    DataTypes and simple basic qustions

---
+++ DataType.java
@@ -0,0 +1,20 @@
+public class DataType {
+    public static void main(String[]args){
+        int a = 10;
+        float price = 14.09f;
+        char c ='p';
+        boolean b = true;
+        byte byt = 8;
+        short n = 1409;
+
+        //long and double its for int and float
+
+        System.out.println(a);
+        System.out.println(price);
+        System.out.println(c);
+        System.out.println(b);
+        // System.out.println(a);
+        System.out.println(byt);
+        System.out.println(n);
+    }
+}
